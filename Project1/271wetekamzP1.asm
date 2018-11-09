0062fa80257d58f421912e45f6c0ac4fee7784e1
Zack <43650976+zwetekamm@users.noreply.github.com>
2018-11-08 22:22:16 -0600

    Format changes

--- Project1/271wetekamzP1.asm
+++ Project1/271wetekamzP1.asm
@@ -10,22 +10,22 @@ TITLE CS271 Program 1   (271wetekamzP1.asm)
 INCLUDE Irvine32.inc
 
 .data
-programTitle		BYTE		"Programming Assignment 1			Author: Zachary Wetekamm", 0
-instructions		BYTE		"Enter two numbers, and I'll show their sum, difference, product, quotient, and remainder.", 0
-prompt_number1		BYTE		"What is the first number? ", 0
-prompt_number2		BYTE		"What is the second number? ", 0
+programTitle        BYTE		"Programming Assignment 1			Author: Zachary Wetekamm", 0
+instructions        BYTE		"Enter two numbers, and I'll show their sum, difference, product, quotient, and remainder.", 0
+prompt_number1      BYTE		"What is the first number? ", 0
+prompt_number2      BYTE		"What is the second number? ", 0
 number1             DWORD		?
 number2             DWORD		?
 sum                 DWORD		?
-difference			DWORD		?
+difference          DWORD		?
 product             DWORD		?
-quotient			DWORD		?
-remainder			DWORD		?
-equalSign			BYTE		" = ", 0
+quotient            DWORD		?
+remainder           DWORD		?
+equalSign           BYTE		" = ", 0
 sumSign             BYTE		" + ", 0
-differenceSign		BYTE		" - ", 0
+differenceSign      BYTE		" - ", 0
 productSign         BYTE		" * ", 0
-quotientSign		BYTE		" / ", 0
+quotientSign        BYTE		" / ", 0
 remainderString     BYTE		" remainder ", 0
 goodbye             BYTE		"Impressed? Bye!", 0
 
@@ -34,117 +34,117 @@ main PROC
 
 ; INTRODUCTION
     ; Display program title, author, and instructions
-	mov	        edx, OFFSET		programTitle
-	call		WriteString
-	call		CrLf
-	call		CrLf
-	mov			edx, OFFSET		instructions
-	call		WriteString
-	call		CrLf
+	mov         edx, OFFSET		programTitle
+	call        WriteString
+	call        CrLf
+	call        CrLf
+	mov         edx, OFFSET		instructions
+	call        WriteString
+	call        CrLf
 
 ; GET DATA
 	; Get number1 from user
-	mov			edx, OFFSET		prompt_number1
-	call		WriteString
-	call		ReadInt
-	mov			number1, eax
+	mov         edx, OFFSET		prompt_number1
+	call        WriteString
+	call        ReadInt
+	mov         number1, eax
 
 	; Get number2 from user
-	mov			edx, OFFSET		prompt_number2
-	call		WriteString
-	call		ReadInt
-	mov			number2, eax
-	call		CrLf
+	mov         edx, OFFSET		prompt_number2
+	call        WriteString
+	call        ReadInt
+	mov         number2, eax
+	call        CrLf
 
 ; CALCULATIONS
 	; Sum
-	mov			eax, number1
-	mov			ebx, number2
-	add			eax, ebx
-	mov			sum, eax
+	mov         eax, number1
+	mov         ebx, number2
+	add         eax, ebx
+	mov         sum, eax
 
 	; Difference
-	mov			eax, number1
-	mov			ebx, number2
-	sub			eax, ebx
-	mov			difference, eax
+	mov         eax, number1
+	mov         ebx, number2
+	sub         eax, ebx
+	mov         difference, eax
 
 	; Product
-	mov			eax, number1
-	mul			number2
-	mov			product, eax
+	mov         eax, number1
+	mul         number2
+	mov         product, eax
 
 	; Quotient and remainder
-	mov			edx, 0
-	mov			eax, number1
-	mov			ebx, number2
-	div			ebx
-	mov			quotient, eax
-	mov			remainder, edx
+	mov         edx, 0
+	mov         eax, number1
+	mov         ebx, number2
+	div         ebx
+	mov         quotient, eax
+	mov         remainder, edx
 
 ; DISPLAY RESULTS
 	; Display sum
-	mov			eax, number1
-	call		WriteDec
-	mov			edx, OFFSET		sumSign
-	call		WriteString
-	mov			eax, number2
-	call		WriteDec
-	mov			edx, OFFSET		equalSign
-	call		WriteString
-	mov			eax, sum
-	call		WriteDec
-	call		CrLf
+	mov         eax, number1
+	call        WriteDec
+	mov         edx, OFFSET		sumSign
+	call        WriteString
+	mov         eax, number2
+	call        WriteDec
+	mov         edx, OFFSET		equalSign
+	call        WriteString
+	mov         eax, sum
+	call        WriteDec
+	call        CrLf
 
 	; Display difference
-	mov			eax, number1
-	call		WriteDec
-	mov			edx, OFFSET		differenceSign
-	call		WriteString
-	mov			eax, number2
-	call		WriteDec
-	mov			edx, OFFSET		equalSign
-	call		WriteString
-	mov			eax, difference
-	call		WriteInt
-	call		CrLf
+	mov         eax, number1
+	call        WriteDec
+	mov         edx, OFFSET		differenceSign
+	call        WriteString
+	mov         eax, number2
+	call        WriteDec
+	mov         edx, OFFSET		equalSign
+	call        WriteString
+	mov         eax, difference
+	call        WriteInt
+	call        CrLf
 
 	; Display product
-	mov			eax, number1
-	call		WriteDec
-	mov			edx, OFFSET		productSign
-	call		WriteString
-	mov			eax, number2
-	call		WriteDec
-	mov			edx, OFFSET		equalSign
-	call		WriteString
-	mov			eax, product
-	call		WriteDec
-	call		CrLf
+	mov         eax, number1
+	call        WriteDec
+	mov         edx, OFFSET		productSign
+	call        WriteString
+	mov         eax, number2
+	call        WriteDec
+	mov         edx, OFFSET		equalSign
+	call        WriteString
+	mov         eax, product
+	call        WriteDec
+	call        CrLf
 
 	; Display quotient and remainder
-	mov			eax, number1
-	call		WriteDec
-	mov			edx, OFFSET		quotientSign
-	call		WriteString
-	mov			eax, number2
-	call		WriteDec
-	mov			edx, OFFSET		equalSign
-	call		WriteString
-	mov			eax, quotient
-	call		WriteDec
-	mov			edx, OFFSET		remainderString
-	call		WriteString
-	mov			eax, remainder
-	call		WriteDec
-	call		CrLf
+	mov         eax, number1
+	call        WriteDec
+	mov         edx, OFFSET		quotientSign
+	call        WriteString
+	mov         eax, number2
+	call        WriteDec
+	mov         edx, OFFSET		equalSign
+	call        WriteString
+	mov         eax, quotient
+	call        WriteDec
+	mov         edx, OFFSET		remainderString
+	call        WriteString
+	mov         eax, remainder
+	call        WriteDec
+	call        CrLf
 
 ; GOODBYE MESSAGE
-	call		CrLf
-	mov			edx, OFFSET		goodbye
-	call		WriteString
-	call		CrLf
-	call		CrLf
+	call        CrLf
+	mov         edx, OFFSET		goodbye
+	call        WriteString
+	call        CrLf
+	call        CrLf
 
 	exit
 main ENDP
